219c3372f69f77f832562cc074557420adadf271
lizebin <bin.ze@shopcider.com>
2022-02-17 14:27:45 +0800

    feat:assembler

---
+++ ddd-demo-app/src/main/java/com/lzb/demo/app/order/assembler/OrderAssembler.java
@@ -0,0 +1,11 @@
+package com.lzb.demo.app.order.assembler;
+
+/**
+ * Dto <-> Entity<br/>
+ * Created on : 2022-02-17 14:26
+ *
+ * @author lizebin
+ */
+public class OrderAssembler {
+
+}
---
+++ ddd-demo-app/src/main/java/com/lzb/demo/app/product/ProductAssemble.java
@@ -0,0 +1,11 @@
+package com.lzb.demo.app.product;
+
+/**
+ * Dto <-> Entity<br/>
+ * Created on : 2022-02-17 14:27
+ *
+ * @author lizebin
+ */
+public class ProductAssemble {
+
+}
