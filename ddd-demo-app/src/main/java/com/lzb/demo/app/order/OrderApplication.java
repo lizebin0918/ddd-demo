51401344bd3ba87ee56394952ad74fc7f4ec3b06
lizebin <lizebin0918@gmail.com>
2022-03-01 07:00:37 +0800

    feat:cqrs分离

---
+++ ddd-demo-app/src/main/java/com/lzb/demo/app/order/OrderApplication.java
@@ -0,0 +1,11 @@
+package com.lzb.demo.app.order;
+
+/**
+ * 订单（写）操作<br/>
+ * Created on : 2022-03-01 06:56
+ *
+ * @author lizebin
+ */
+public class OrderApplication {
+
+}
---
+++ ddd-demo-app/src/main/java/com/lzb/demo/app/order/OrderQueryApplication.java
@@ -0,0 +1,11 @@
+package com.lzb.demo.app.order;
+
+/**
+ * <br/>
+ * Created on : 2022-03-01 06:57
+ *
+ * @author lizebin
+ */
+public class OrderQueryApplication {
+
+}
