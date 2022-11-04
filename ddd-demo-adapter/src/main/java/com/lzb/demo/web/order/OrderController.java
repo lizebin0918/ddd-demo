842bd93fa00d06083da7cc9d46916333ffc32fe7
lizebin <lizebin0918@gmail.com>
2022-11-04 11:50:32 +0800

    feat:打包插件修改

--- ddd-demo-adapter/src/main/java/com/lzb/demo/web/order/OrderController.java
+++ ddd-demo-adapter/src/main/java/com/lzb/demo/web/order/OrderController.java
@@ -1,5 +1,6 @@
 package com.lzb.demo.web.order;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo/order")
 public class OrderController {
 
+	@GetMapping("")
+	public String helloWorld() {
+		return "Hello world";
+	}
 }
--- start/src/main/java/com/lzb/demo/Application.java
+++ start/src/main/java/com/lzb/demo/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author lizebin
  */
+@EnableWebMvc
 @EnableRetry
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
