ceb9bdc80d8ffce7c181ef2a2ece93d59a908e27
lizebin <lizebin0918@gmail.com>
2023-01-19 18:55:58 +0800

    feat:Spring test也能跑

---
+++ start/src/test/java/com/lzb/demo/test/MySpringTest.java
@@ -0,0 +1,18 @@
+package com.lzb.demo.test;
+
+import com.lzb.demo.SpringbootTestBase;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <br/>
+ * Created on : 2023-01-19 18:53
+ * @author lizebin
+ */
+public class MySpringTest extends SpringbootTestBase {
+
+    @Test
+    public void should_() {
+        System.out.println("MySpring Test");
+    }
+
+}
